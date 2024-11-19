357910a1168ffab08a95332ead573bdd67fb3440
SpiderIllanes <ironmanmark152@gmail.com>
2024-11-19 12:15:01 -0400

    se agrego numeros

---
+++ JS/main.js
@@ -0,0 +1,35 @@
+let mensaje = "Hola Mundo";
+
+function sumar(){
+let numerouno = 3;
+let numerodos= 4;
+
+
+let suma = numerouno + numerodos;
+
+let multiplicacion = numerouno * numerodos;
+let division = numerouno / numerodos;
+
+alert("La suma es: "+suma);
+}
+function restar(){
+let numerouno = 3;
+let numerodos= 4;
+let resta = numerouno - numerodos;
+alert("La resta es: "+resta);
+
+}
+function multiplicacion(){
+    let numerouno = 3;
+    let numerodos= 4;
+    let multiplicacion = numerouno * numerodos;
+    alert("La multiplicacion es: "+multiplicacion);
+    
+    }
+    function division(){
+        let numerouno = 3;
+        let numerodos= 4;
+        let division = numerouno / numerodos;
+        alert("La division es: "+division);
+        
+        }
